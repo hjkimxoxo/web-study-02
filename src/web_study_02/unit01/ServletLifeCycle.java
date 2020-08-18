16d1d84063d8c39eedcbc8870dce7f5cfbbfde62
hjkim <pjt911219@gmail.com>
2020-08-18 10:42:27 +0900

    서블릿 생명주기확인

---
+++ src/web_study_02/unit01/ServletLifeCycle.java
@@ -0,0 +1,46 @@
+package web_study_02.unit01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLifeCycle
+ */
+@WebServlet("/LifeCycle")
+public class ServletLifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public ServletLifeCycle() {
+        super();
+  
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init()");
+	}
+
+	
+	public void destroy() {
+		System.out.println("destroy()");
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
